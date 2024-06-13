c92d117bd11c77b9daea5f889116d11b5a403e0a
121kingrohit121kinglaxus <121558155+121kingrohit121kinglaxus@users.noreply.github.com>
2024-06-13 16:44:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@
 
 module.exports = {
   TOKEN: "",
-  ownerID: ["1004206704994566164", ""], 
+  ownerID: ["974258387049123840", ""], 
   setupFilePath: './commands/setup.json',
   commandsDir: './commands',  
   embedColor: "00fbff",
