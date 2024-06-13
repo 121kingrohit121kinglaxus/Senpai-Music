fe2567f05963b8d7919791180d88888f1af50456
121kingrohit121kinglaxus <121558155+121kingrohit121kinglaxus@users.noreply.github.com>
2024-06-13 16:49:07 +0530

    Update support.js

--- commands/support.js
+++ commands/support.js
@@ -29,12 +29,12 @@ module.exports = {
   run: async (client, interaction) => {
     try {
 
-      const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-      const githubLink = "https://github.com/GlaceYT";
-      const replitLink = "https://replit.com/@GlaceYT";
-      const youtubeLink = "https://www.youtube.com/@GlaceYT";
+      const supportServerLink = "https://discord.com/invite/kawaiicorner";
+      const githubLink = "https://github.com/";
+      const replitLink = "https://replit.com/";
+      const youtubeLink = "https://www.youtube.com/";
         const embed = new EmbedBuilder()
-            .setColor('#b300ff')
+            .setColor('#b23ff4')
             .setAuthor({
               name: 'Support Server',
               iconURL: 'https://cdn.discordapp.com/attachments/1230824451990622299/1230824519220985896/6280-2.gif?ex=6638ae28&is=66375ca8&hm=13e4a1b91a95b2934a39de1876e66c11711c7b30ac1a91c2a158f2f2ed1c2fc6&', 
