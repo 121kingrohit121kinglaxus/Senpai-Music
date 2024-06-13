0a43accdfb945c6bcb8f10d99a69b852ac270b81
121kingrohit121kinglaxus <121558155+121kingrohit121kinglaxus@users.noreply.github.com>
2024-06-13 16:46:56 +0530

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -32,8 +32,8 @@ module.exports = {
      
 
       const embed = new EmbedBuilder()
-         .setColor('#0099ff')
-      .setTitle('💎 Prime Music Bot')
+         .setColor('#b23ff4')
+      .setTitle('🌸 Senpai Music Bot')
       .setDescription('Welcome to the Music Bot!\n\n- Here are the available commands:\n\n' +
         '**/play :** Start playing the songs.\n' +
         '**/ping :** check bot latency.\n' +
